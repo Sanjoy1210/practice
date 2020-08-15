1e7b9a3a67dcbc36f28e2f2f17bd438123af164c
Pappu <sanjoy.paul@hotmail.com>
2020-08-15 20:51:05 +0600

    add a new function called myfunc

--- Hello.c
+++ Hello.c
@@ -5,6 +5,11 @@ void printHello()
     printf("Hello, Git. I am learnig git\n");
 }
 
+void myFunc()
+{
+    printf("Wow, I am excited to learn git\n");
+}
+
 int main()
 {
     printf("Hello, World\n");
